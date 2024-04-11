5b981780fb75a72dd13568f94cb4e4c7f44bd225
arvind <arvind6599@gmail.com>
2024-04-11 16:48:48 +0200

    regression script

---
+++ prediction_model.R
@@ -0,0 +1,23 @@
+library(ggplot2)
+
+data <- read.csv('beerhall.csv', header=TRUE)
+
+colnames(data) <- c("County", "Region", "Region_Code", "Criminals_per_100k", 
+                    "Ale_Beer_Houses_per_100k", "Attendants_at_School_per_10k", 
+                    "Attendants_at_Public_Worship_per_2000")
+# Load necessary library for regression modeling
+library(ggplot2)
+
+# Fit a multiple linear regression model
+model <- lm(Criminals_per_100k ~ Ale_Beer_Houses_per_100k + 
+              Attendants_at_School_per_10k + 
+              Attendants_at_Public_Worship_per_2000, data = data)
+
+# Summarize the model
+summary(model)
+
+# Make predictions
+predictions <- predict(model, newdata = data)
+
+# Print the predictions
+print(predictions)
